01558d4c1db06dafd51445877d3548200c6773dd
jay-bulk <jay.bulk@tuta.io>
2024-02-21 23:54:38 -0700

    refactor: remove progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+use anyhow::{Context, Result};
 use clap::Parser;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ApiConfig {
+    client_name: String,
+    client_id: String,
+    client_secret: String, //Endpoints...
+                           //Queries...
+}
 
 #[derive(Parser)]
 struct Cli {
@@ -6,17 +15,24 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), io::Error> {
     let args = Cli::parse();
+    let pb = indicatif::ProgressBar::new(100);
 
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-    match content {
-        Ok(content) => content,
-        Err(error) => {
-            return Err(error.info());
-        }
-    };
+    for i in 0..100 {
+        pb.println(format!("[+] finished #{}", i));
+        pb.inc(1);
+    }
+    pb.finish_with_message("done");
 
-    println!("file conent: {}", content);
+    let config = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+
+    let cfg: ApiConfig;
+    if (args.length() > 1) {
+        cfg = confy::load("rurl", config);
+    } else {
+        cfg = confy::load("rurl", None)?;
+    }
     Ok(())
 }
